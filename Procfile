798821df7b64708fd20f9e3ad8bcc6845ce13b1d
Somushiva162124 <somushivakumar057@gmail.com>
2025-04-24 18:14:23 +0530

    Deploy to Render

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn online_learning.wsgi:application --worker-class gevent --workers 2 --threads 2 --bind 0.0.0.0:$PORT
+web: gunicorn online_learning.wsgi:application --worker-class gevent --workers 1 --threads 1 --bind 0.0.0.0:$PORT
--- online_learning/urls.py
+++ online_learning/urls.py
@@ -2,22 +2,26 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from core.views import maintenance
+from core.views import maintenance  # Make sure your maintenance view is correct
 
-urlpatterns = []
+urlpatterns = [
+    # Maintenance Mode URL should come first
+    path('', maintenance),  # If maintenance is enabled, this will block access to other pages.
 
-if settings.MAINTENANCE_MODE:
-    urlpatterns += [
-        path('', maintenance),
-    ]
-else:
-    urlpatterns += [
-        path('', include('core.urls')),
-        path('admin/', admin.site.urls),
-        path('accounts/', include('django.contrib.auth.urls')),
-        path('ckeditor5/', include('django_ckeditor_5.urls')),
-    ]
+    # Admin
+    path('admin/', admin.site.urls),
 
+    # Core app URLs
+    path('', include('core.urls')),  # Main app's URL configuration
+
+    # Auth URLs (This includes login, logout, etc.)
+    path('accounts/', include('django.contrib.auth.urls')),
+
+    # CKEditor URL
+    path('ckeditor5/', include('django_ckeditor_5.urls')),
+]
+
+# Serving media and static files during development
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
